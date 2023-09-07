e06a4a14f4bb5bcf56c5ace4df8c4e110667ced9
Jeremy <marszalekbcn@gmail.com>
2023-09-07 23:42:53 +0200

    homepage

--- src/Router.js
+++ src/Router.js
@@ -11,15 +11,10 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/projet_arbitrage">
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route
-            exact
-            path="https://marszalekj.github.io/projet_arbitrage/"
-            element={<Home />}
-          />
           <Route exact path="/home" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/product/:id" element={<Product />} />
